086001e3027f2016a161d0fedcbc76bb8f9e77c4
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2023-01-20 14:08:24 -0400

    Add unit tests for PasswordValidator

--- app/src/main/java/br/com/wesjon/testworkshopjan2023/PasswordValidator.kt
+++ app/src/main/java/br/com/wesjon/testworkshopjan2023/PasswordValidator.kt
@@ -1,10 +1,15 @@
 package br.com.wesjon.testworkshopjan2023
 
 class PasswordValidator {
-    companion object{
-        private const val MIN_PASSWORD_LENGTH = 8
+    companion object {
+        private val validations = listOf(
+            "[\\w\\W]{8,}",
+            "[A-Z]",
+            "[a-z]",
+            "[0-9]",
+            "[_\\W]"
+        ).map { it.toRegex() }
     }
-    fun isValid(password: String): Boolean {
-        return password.length >= MIN_PASSWORD_LENGTH
-    }
+
+    fun isValid(password: String) = validations.all { password.contains(it) }
 }
